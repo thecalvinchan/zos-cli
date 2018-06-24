2aaaf343a0592aa395aac0313ee777edcf4b0a0a
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-06-24 20:47:46 -0300

    Add pull command

--- src/bin/program.js
+++ src/bin/program.js
@@ -14,6 +14,7 @@ const bump = require('../commands/bump')
 const upgrade = require('../commands/upgrade')
 const link = require('../commands/link')
 const status = require('../commands/status')
+const pull = require('../commands/pull')
 const compare = require('../commands/compare')
 const freeze = require('../commands/freeze')
 
@@ -21,14 +22,14 @@ program
   .name('zos')
   .usage('<command> [options]')
   .description('where <command> is one of:\n' +
-          '\t add, bump, create, init, link, push, status, compare, upgrade.')
+          '\t add, bump, create, init, link, push, status, compare, pull, upgrade.')
   .version(version, '--version')
   .option('-v, --verbose', 'verbose mode on: output errors stacktrace and detailed log.')
   .option('-s, --silent', 'silent mode: no output sent to stderr.')
   .on('option:verbose', () => { Logger.verbose(true); } )
   .on('option:silent', () => { Logger.silent(true); } )
 
-const commands = [init, add, push, create, bump, upgrade, link, status, compare, freeze]
+const commands = [init, add, push, create, bump, upgrade, link, status, compare, pull, freeze]
 
 commands.forEach((c) => c.register(program));
 
